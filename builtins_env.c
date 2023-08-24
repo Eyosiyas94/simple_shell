779c9f9eb5a0b1f5d50c4312e19667c710fe6289
Eyosiyas94 <134216719+Eyosiyas94@users.noreply.github.com>
2023-08-24 12:52:20 +0300

    b

--- builtins_env.c
+++ builtins_env.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- * builtin_env - displays env where the shell runs
- * @data: structure data type for the data
- * Return: 0 if sucess, or other number if its declared in the arguments
+ * builtin_env - shows the environment where the shell runs
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
 int builtin_env(data_of_program *data)
 {
 	int i;
-	char cp[50] = {'\0'};
-	char *copy = NULL;
+	char cpname[50] = {'\0'};
+	char *var_copy = NULL;
 
 	/* if not arguments */
 	if (data->tokens[1] == NULL)
@@ -17,28 +17,29 @@ int builtin_env(data_of_program *data)
 	else
 	{
 		for (i = 0; data->tokens[1][i]; i++)
-		{
+		{/* checks if exists a char = */
 			if (data->tokens[1][i] == '=')
-			{
-				copy = str_duplicate(env_get_key(cp, data));
-				if (copy != NULL)
-					env_set_key(cp, data->tokens[1] + i + 1, data);
+			{/* checks if exists a var with the same name and change its value*/
+			/* temporally */
+				var_copy = str_duplicate(env_get_key(cpname, data));
+				if (var_copy != NULL)
+					env_set_key(cpname, data->tokens[1] + i + 1, data);
 
 				/* print the environ */
 				print_environ(data);
-				if (env_get_key(cp, data) == NULL)
-				{
+				if (env_get_key(cpname, data) == NULL)
+				{/* print the variable if it does not exist in the environ */
 					_print(data->tokens[1]);
 					_print("\n");
 				}
 				else
-				{
-					env_set_key(cp, copy, data);
-					free(copy);
+				{/* returns the old value of the var*/
+					env_set_key(cpname, var_copy, data);
+					free(var_copy);
 				}
 				return (0);
 			}
-			cp[i] = data->tokens[1][i];
+			cpname[i] = data->tokens[1][i];
 		}
 		errno = 2;
 		perror(data->command_name);
@@ -49,11 +50,12 @@ int builtin_env(data_of_program *data)
 
 /**
  * builtin_set_env - ..
- * @data: structure data type for the date
- * Return: 0 if sucess, or other number if its declared in the arguments
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
  */
 int builtin_set_env(data_of_program *data)
 {
+	/* validate args */
 	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
 		return (0);
 	if (data->tokens[3] != NULL)
@@ -70,11 +72,12 @@ int builtin_set_env(data_of_program *data)
 
 /**
  * builtin_unset_env - ..
- * @data: structure data type for the program data
+ * @data: struct for the program's data'
  * Return: ..
  */
 int builtin_unset_env(data_of_program *data)
 {
+	/* validate args */
 	if (data->tokens[1] == NULL)
 		return (0);
 	if (data->tokens[2] != NULL)
